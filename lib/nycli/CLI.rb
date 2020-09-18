26b47c11bbb774b96adcffe75a9c07b208c3c53d
Roeck <rons9n@gmail.com>
2020-09-18 15:34:25 -0400

    initial cli

--- lib/nycli/CLI.rb
+++ lib/nycli/CLI.rb
@@ -1,3 +1,49 @@
 class NYCLI::CLI
-    puts "Hello"
+
+# Color variables:
+
+  @@blu = "\e[1;34m"
+  @@green = "\e[1;32m"
+  @@white = "\e[0m"
+  @@yellow = "\e[1;33m"
+  @@red = "\e[1;31m"
+
+  # Menu:
+
+   puts "#{@@blu}
+  .~~~~~~~~~~~~~~~~.
+  | #{@@yellow}WELCOME TO NYC!#{@@blu}|
+  .~~~~~~~~~~~~~~~~.
+  #{@@green}\n"
+
+  puts "\n#{@@green}Choose your desired events by their numbers:#{@@white}\n\n"
+
+  puts "numbered events"
+
+  # Show more events option:
+
+  def self.user_prompt
+    puts "\n#{@@green}To view more events, type 'more'#{@@white}"
+  end
+
+  def self.action(input)
+    if input == 'more'
+      puts "show more events"
+
+    else
+      puts "\n#{@@red}Invalid input. Try again!#{@@white}"
+    end
+  end
+
+  # Exit program:
+  
+  puts "\n#{@@yellow}Type 'exit' to quit."
+  NYCLI::CLI.user_prompt
+  input = gets.strip
+
+  while input != 'exit'
+    NYCLI::CLI.action(input)
+    input = gets.strip
+  end
+      puts "\n#{@@yellow}Goodbye!#{@@white}\n\n"
 end
